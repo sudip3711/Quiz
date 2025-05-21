671f147ee1bf7d6823b26eebb02069f820c7ce0e
sudip3711 <149355694+sudip3711@users.noreply.github.com>
2025-05-21 11:25:34 +0530

    commit-4

--- src/main/java/com/sudip/Quiz/Repo/QuizRepo.java
+++ src/main/java/com/sudip/Quiz/Repo/QuizRepo.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface QuizRepo extends JpaRepository<Quiz,Integer> {
 
 //    @Query("SELECT q FROM Quiz q WHERE q.createdBy.id = :id")
